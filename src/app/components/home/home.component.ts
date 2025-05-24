b0237c42e3fabf15e2a7e41d16d6fda67a525ed6
Mahmoud Elmasry <me653830@gmail.com>
2025-05-24 21:29:55 +0300

    finished home page ui design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent,HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
