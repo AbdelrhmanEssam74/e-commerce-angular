3c30c627ac75050e41cafccee657315620703171
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-28 16:36:53 +0300

    Add login and checkout routes; protect checkout with AuthGuard; fix checkout component import path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {CheckoutComponent} from './components/chekcout/checkout.component';
+import { AuthGuard } from './auth.guard';
 // import { CartComponent } from './components/cart/cart.component';
 // import { WishlistComponent } from './components/wishlist/wishlist.component';
 
@@ -7,5 +10,11 @@ export const routes: Routes = [
   {path:'' , component:HomeComponent},
   // {path:'cart' , component:CartComponent},
   // {path:'wishlist' , component:WishlistComponent},
-  {path:'home', component: HomeComponent, title: 'home' }
+  {path:'home', component: HomeComponent, title: 'home' },
+  {
+    path: 'login',component:LoginComponent
+  },
+  {
+    path:'checkout',component:CheckoutComponent, canActivate: [AuthGuard]
+  }
 ];
