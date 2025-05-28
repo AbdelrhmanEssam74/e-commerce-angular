b91a83a86761bb2e891a488b9451ba58f6c78a31
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-29 01:49:49 +0300

    Add register route to app routes and fix formatting inconsistencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {LoginComponent} from './components/login/login.component';
 import {CheckoutComponent} from './components/chekcout/checkout.component';
-import { AuthGuard } from './auth.guard';
+import {AuthGuard} from './auth.guard';
+import {RegisterComponent} from './components/register/register.component';
 // import { CartComponent } from './components/cart/cart.component';
 // import { WishlistComponent } from './components/wishlist/wishlist.component';
 
 export const routes: Routes = [
-  {path:'' , component:HomeComponent},
+  {path: '', component: HomeComponent},
   // {path:'cart' , component:CartComponent},
   // {path:'wishlist' , component:WishlistComponent},
-  {path:'home', component: HomeComponent, title: 'home' },
+  {path: 'home', component: HomeComponent, title: 'home'},
   {
-    path: 'login',component:LoginComponent
+    path: 'login', component: LoginComponent
   },
   {
-    path:'checkout',component:CheckoutComponent, canActivate: [AuthGuard]
+    path: 'checkout', component: CheckoutComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: 'register', component: RegisterComponent
   }
 ];
