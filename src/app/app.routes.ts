d58a95ae0f161068531e2f60602fdc7ce684b85d
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-29 05:46:24 +0300

    Enable routing for Cart and Wishlist components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,13 @@ import {LoginComponent} from './components/login/login.component';
 import {CheckoutComponent} from './components/chekcout/checkout.component';
 import {AuthGuard} from './auth.guard';
 import {RegisterComponent} from './components/register/register.component';
-// import { CartComponent } from './components/cart/cart.component';
-// import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { CartComponent } from './components/cart/cart.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  // {path:'cart' , component:CartComponent},
-  // {path:'wishlist' , component:WishlistComponent},
+  {path:'cart' , component:CartComponent},
+  {path:'wishlist' , component:WishlistComponent},
   {path: 'home', component: HomeComponent, title: 'home'},
   {
     path: 'login', component: LoginComponent
