bedda25cd77ab279ab791b71145341d8398399fd
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-29 05:39:19 +0300

    Enhance AddressList component with event handling and improved UX
    - Add `selectAddress` method and emit event on address click
    - Display a message when no addresses are available
    - Refactor `ngOnInit` to retrieve user and load addresses using AddressService
    - Update HTML to include conditional rendering and event binding

--- src/app/components/address-list/address-list.component.ts
+++ src/app/components/address-list/address-list.component.ts
@@ -1,28 +1,43 @@
-import {Component} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {NgForOf} from '@angular/common';
-import {Address} from '../../interface/address.model';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { AddressService } from '../../services/address.service';
+import { Address } from '../../interface/address.model';
 import {AddressItemComponent} from '../address-item/address-item.component';
 
 @Component({
   selector: 'app-address-list',
-  imports: [
-    NgForOf,
-    AddressItemComponent
-  ],
   templateUrl: './address-list.component.html',
-  styleUrl: './address-list.component.css'
+  styleUrl: './address-list.component.css',
+  imports: [
+    AddressItemComponent
+  ]
 })
-export class AddressListComponent {
-  constructor(private http: HttpClient) {
-  }
-
+export class AddressListComponent implements OnInit {
+  user: any = null;
   addresses: Address[] = [];
 
+  @Output() addressSelected = new EventEmitter<number>();
+
+  selectAddress(id: number) {
+    this.addressSelected.emit(id);
+  }
+
+  constructor(private addressService: AddressService) {}
+
   ngOnInit() {
-    this.http.get('http://localhost:3000/api/address/1').subscribe((data: any) => {
-      this.addresses = data;
-      console.log(this.addresses);
-    })
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.user = JSON.parse(storedUser);
+
+      if (this.user.id) {
+
+        this.addressService.addresses$.subscribe(addresses => {
+          this.addresses = addresses;
+        });
+
+
+        this.addressService.loadAddresses(this.user.id);
+
+      }
+    }
   }
 }
